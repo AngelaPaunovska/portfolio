927a4ccb9bd4a25ec66167d0308f91064fb82af6
angelapaunovskastojanovska <angelapaunovskastojanovska894@gmail.com>
2026-06-09 19:44:41 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { resolve } from "path";
+import { defineConfig } from 'vite'
+ 
+export default defineConfig({
+
+    root: "src",
+
+    build: {
+    emptyOutDir: true,
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+       // projects: revolve(__dirname, "src/pages/projectOne.html"),
+      },
+    },
+    outDir: "../build",
+  },
+
+  base: '/portfolio/',
+
+});
+ 
