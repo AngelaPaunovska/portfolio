937deec0f1899712f496e3be7b2acbd55dd4a817
angelapaunovskastojanovska <angelapaunovskastojanovska894@gmail.com>
2026-06-25 14:19:15 +0200

    Final Project

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,44 @@
 import '../styles/style.css';
 
 
+const cards = document.querySelectorAll(".card, .card1");
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        window.location.href = card.dataset.page;
+    });
+});
+
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
 
 
 
+
+const sections = document.querySelectorAll("section, div#stack, footer");
+const dots = document.querySelectorAll(".dot");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        dots.forEach((dot) => dot.classList.remove("active"));
+
+        const id = entry.target.id;
+        const activeDot = document.querySelector(`.dot[href="#${id}"]`);
+
+        if (activeDot) activeDot.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.6
+  }
+);
+
+sections.forEach((section) => observer.observe(section));
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,15 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-       // projects: revolve(__dirname, "src/pages/projectOne.html"),
+        contact: resolve(__dirname, "src/pages/contact.html"),
+       projects: resolve(__dirname, "src/pages/projects.html"),
+       project1: resolve(__dirname, "src/pages/project1.html"),
+       project2: resolve(__dirname, "src/pages/project2.html"),
+       project3: resolve(__dirname, "src/pages/project3.html"),
+       project4: resolve(__dirname, "src/pages/project4.html"),
+       project5: resolve(__dirname, "src/pages/project5.html"),
+       project6: resolve(__dirname, "src/pages/project6.html"),
+       
       },
     },
     outDir: "../build",
